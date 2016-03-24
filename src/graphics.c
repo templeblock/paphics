30a829ccaa6c0b96b1b62c2ae2c16ee2cc53b231
Sancho-Panza <leo@ndrs.fr>
2016-03-24 23:00:12 +0100

    Add error_quit() and init_graphics() to graphics

--- src/graphics.c
+++ src/graphics.c
@@ -1,1 +1,23 @@
 #include "graphics.h"
+
+void error_quit() {
+    SDL_Quit();
+    exit(EXIT_FAILURE);
+}
+
+void init_graphics(Uint32 flags) {
+
+    if (SDL_WasInit(flags)) {
+        fprintf(stderr, "\nCall to init_graphics with at least one flag already initialized\n");
+        error_quit();
+    } else {
+
+        SDL_SetMainReady();
+
+        if (SDL_Init(flags) == -1) {
+            fprintf(stderr, "\nUnable to initialize SDL: %s\n", SDL_GetError());
+            error_quit();
+        }
+    }
+
+}
--- src/graphics.h
+++ src/graphics.h
@@ -1,8 +1,11 @@
 #ifndef DEF_GRAPHICS_H
 #define DEF_GRAPHICS_H
 
+#include <stdio.h>
 #include <SDL2/SDL.h>
 
-void demo();
+void error_quit();
+
+void init_graphics(Uint32 flags);
 
 #endif
