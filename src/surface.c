87c1bc4a60609cbca262670b4f5cf5f769cd9973
Sancho-Panza <leo@ndrs.fr>
2016-03-26 14:56:43 +0100

    Add load_surface_img

--- src/surface.c
+++ src/surface.c
@@ -1,1 +1,20 @@
 #include "surface.h"
+
+void load_surface_img(Surface surface, char* pathToImg) {
+
+    SDL_Surface* tmp;
+
+    tmp = NULL;
+
+    tmp = IMG_Load(pathToImg);
+
+    if (tmp == NULL) {
+        fprintf(stderr, "\nUnable to load image %s! SDL Error: %s\n", pathToImg, SDL_GetError());
+        error_quit();
+    }
+
+    SDL_BlitSurface(tmp, NULL, surface.surface, NULL );
+
+    SDL_FreeSurface(tmp);
+
+}
--- src/surface.h
+++ src/surface.h
@@ -2,9 +2,13 @@
 #define DEF_SURFACE_H
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "graphics.h"
 
 typedef struct {
     SDL_Surface* surface;
 } Surface;
 
 #endif
+
+void load_surface_img(Surface surface, char* pathToImg);
