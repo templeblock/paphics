705dab0a0265f25bb5500a72c1df850341a9a635
Sancho-Panza <leo@ndrs.fr>
2016-03-26 23:40:06 +0100

    Add draw_fill_rectangle

--- src/draw.c
+++ src/draw.c
@@ -1,5 +1,45 @@
 #include "draw.h"
 
+void draw_fill_rectangle(Surface surface, Point a, Point b, Uint32 color) {
+
+
+    int i;
+    Point top_left;
+    Point bottom_left;
+    Point top_right;
+    Point bottom_tmp;
+    Point top_tmp;
+
+    if (a.x <= b.x) {
+        bottom_left.x = a.x;
+        top_left.x = a.x;
+        top_right.x = b.x;
+    } else {
+        bottom_left.x = b.x;
+        top_left.x = b.x;
+        top_right.x = a.x;
+    }
+
+    if (a.y <= b.y) {
+        bottom_left.y = a.y;
+        top_right.y = b.y;
+        top_left.y = b.y;
+    } else {
+        bottom_left.y = b.y;
+        top_right.y = a.y;
+        top_left.y = a.y;
+    }
+
+    top_tmp = top_left;
+    bottom_tmp = bottom_left;
+
+    for (i = 0; i < top_right.x - bottom_left.x; i++) {
+        bottom_tmp.x += 1; // replacing 1 per i gives a funny effect
+        top_tmp.x += 1;
+        draw_line(surface, bottom_tmp, top_tmp, color);
+    }
+}
+
 void draw_line(Surface surface, Point a, Point b, Uint32 color) {
 
     Point min;
--- src/draw.h
+++ src/draw.h
@@ -6,6 +6,8 @@
 #include "point.h"
 #include "graphics.h"
 
+void draw_fill_rectangle(Surface surface, Point a, Point b, Uint32 color);
+
 void draw_line(Surface surface, Point a, Point b, Uint32 color);
 
 void draw_rectangle(Surface surface, Point a, Point b, Uint32 color);
