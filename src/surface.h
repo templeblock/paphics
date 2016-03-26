35c9a6f5f778648a3dc8a0295e6e44c11e28d2b6
Sancho-Panza <leo@ndrs.fr>
2016-03-26 17:39:18 +0100

    Add add_pixel and add size to typedef

--- src/surface.h
+++ src/surface.h
@@ -4,11 +4,15 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "graphics.h"
+#include "point.h"
 
 typedef struct {
     SDL_Surface* surface;
+    Point size;
 } Surface;
 
 #endif
+
+void add_pixel(Surface surface, Point point, Uint32 color);
 
 void load_surface_img(Surface surface, char* pathToImg);
