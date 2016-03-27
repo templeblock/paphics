6a64e3aee666983cdcd43fe8e86d3cacb7041ce6
Sancho-Panza <leo@ndrs.fr>
2016-03-27 15:35:03 +0200

    Add point_distance

--- src/point.c
+++ src/point.c
@@ -1,1 +1,12 @@
 #include "point.h"
+
+int point_distance(Point a, Point b) {
+
+    int distance;
+
+    distance = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+
+    distance = (int) sqrt(distance);
+
+    return distance;
+}
--- src/point.h
+++ src/point.h
@@ -1,9 +1,13 @@
 #ifndef DEF_POINT_H
 #define DEF_POINT_H
 
+#include <math.h>
+
 typedef struct {
     int x;
     int y;
 } Point;
+
+int point_distance(Point a, Point b);
 
 #endif
