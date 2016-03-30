da3de5bf61edd00a3c8c78d3310cb35fbf10c437
Sancho-Panza <leo@ndrs.fr>
2016-03-30 20:48:10 +0200

    Update struct Surface, add clear, blit and create surface functions

--- src/surface.c
+++ src/surface.c
@@ -1,5 +1,48 @@
 #include "surface.h"
 
+Surface create_surface(Point size, Point origin, Surface parent) {
+
+    Surface newSurface;
+    SDL_Surface* newSDL_Surface;
+
+    newSDL_Surface = NULL;
+    newSDL_Surface = SDL_CreateRGBSurface(0, size.x, size.y, 32, 0, 0, 0, 0);
+
+    if(newSDL_Surface == NULL) {
+        fprintf(stderr, "CreateRGBSurface failed: %s\n", SDL_GetError());
+        error_quit();
+    }
+
+    newSurface.size = size;
+    newSurface.origin = origin;
+    newSurface.parent = parent.surface;
+    newSurface.surface = newSDL_Surface;
+
+    return newSurface;
+
+}
+
+void blit_surface(Surface surface) {
+
+    SDL_Rect dest_rect;
+
+    dest_rect.w = surface.size.x;
+    dest_rect.h = surface.size.y;
+    dest_rect.x = surface.origin.x;
+    dest_rect.y = surface.origin.y;
+
+    if (SDL_BlitSurface(surface.surface, NULL, surface.parent, &dest_rect) < 0) {
+        fprintf(stderr, "\nBlit failed: %s\n", SDL_GetError());
+        error_quit();
+    }
+
+}
+
+void clear_surface(Surface surface) {
+
+    SDL_FillRect(surface.surface, NULL, 0x000000);
+}
+
 void load_surface_img(Surface surface, char* pathToImg) {
 
     SDL_Surface* tmp;
--- src/surface.h
+++ src/surface.h
@@ -9,7 +9,15 @@
 typedef struct {
     SDL_Surface* surface;
     Point size;
+    Point origin;
+    void* parent;
 } Surface;
+
+Surface create_surface(Point size, Point origin, Surface parent);
+
+void blit_surface(Surface surface);
+
+void clear_surface(Surface surface);
 
 void load_surface_img(Surface surface, char* pathToImg);
 
