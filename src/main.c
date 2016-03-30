81e9b8e9e731b90644475850844a21937dcc102a
Sancho-Panza <leo@ndrs.fr>
2016-03-30 20:49:51 +0200

    Update demo

--- src/main.c
+++ src/main.c
@@ -21,39 +21,46 @@ void demo() {
     init_graphics(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO);
     atexit(SDL_Quit);
 
-    Window window;
-    Point position;
-    Point size;
+    const Point screen_size = get_screen_size();
+    int dx;
+    int dy;
+    int radius;
+    Surface plate;
+    Window w_window;
+    Surface window;
+    Point window_position;
     Event input;
     Uint32 color;
-    int i;
+    Point plate_origin;
+    Point plate_size;
     Sphere sphere1;
     Sphere sphere2;
     Sphere sphere3;
     Sphere sphere4;
 
-
-    position.x = 50;
-    position.y = 50;
-    size.x = 800;
-    size.y = 600;
-
     input = create_event();
-    window = create_window("Démo", position, size, SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_SHOWN);
+    w_window = create_window("Démo", window_position, screen_size, SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_SHOWN);
 
-    sphere1.center.x = window.size.x / 2;
-    sphere1.center.y = window.size.y / 2;
+    window = get_window_surface(w_window);
 
-    int dx;
-    int dy;
-    int radius;
+    plate_size = get_screen_size();
+    plate_size.x /= 2;
+    plate_size.y /= 2;
+
+    plate_origin = get_screen_size();
+    plate_origin.x /= 4;
+    plate_origin.y /= 4;
+
+    plate = create_surface(plate_size, plate_origin, window);
+
+    sphere1.center.x = plate.size.x / 2;
+    sphere1.center.y = plate.size.y / 2;
 
     dx = 1;
     dy = 1;
-    color = 0xEAAAAA;
-    i = 0;
+    color = 0xBAB073;
 
-    radius = 20;
+    radius = 7;
     sphere1.radius = radius;
     sphere2.radius = radius;
     sphere3.radius = radius;
@@ -78,47 +85,43 @@ void demo() {
             dx++;
         }
 
-        if (is_out_of_surface_x(window.surface, sphere1.center.x + dx, sphere1.radius)) {
+        if (is_out_of_surface_x(plate, sphere1.center.x + dx, sphere1.radius)) {
             dx = -dx;
         }
 
-        if (is_out_of_surface_y(window.surface,sphere1.center.y + dy, sphere1.radius)) {
+        if (is_out_of_surface_y(plate,sphere1.center.y + dy, sphere1.radius)) {
             dy = -dy;
         }
 
         sphere1.center.x += dx;
         sphere1.center.y += dy;
-        sphere2.center.x = window.size.x - sphere1.center.x;
-        sphere2.center.y = window.size.y - sphere1.center.y;
+        sphere2.center.x = plate.size.x - sphere1.center.x;
+        sphere2.center.y = plate.size.y - sphere1.center.y;
         sphere3.center.x = sphere1.center.x;
-        sphere3.center.y = window.size.y - sphere1.center.y;
-        sphere4.center.x = window.size.x - sphere1.center.x;
+        sphere3.center.y = plate.size.y - sphere1.center.y;
+        sphere4.center.x = plate.size.x - sphere1.center.x;
         sphere4.center.y = sphere1.center.y;
-
-        if (i == 400) {
-            color += 1234567890;
-            i = 0;
-        } else {
-            i++;
-        }
 
         sphere1.color = color;
         sphere2.color = color;
         sphere3.color = color;
         sphere4.color = color;
 
-        // clear_window(window);
+        clear_surface(window);
+        clear_surface(plate);
 
-        draw_fill_sphere(window.surface, &sphere1);
-        draw_fill_sphere(window.surface, &sphere2);
-        draw_fill_sphere(window.surface, &sphere3);
-        draw_fill_sphere(window.surface, &sphere4);
+        draw_fill_sphere(plate, &sphere1);
+        draw_fill_sphere(plate, &sphere2);
+        draw_fill_sphere(plate, &sphere3);
+        draw_fill_sphere(plate, &sphere4);
 
-        update_window(window);
-        SDL_Delay(2);
+        blit_surface(plate);
+
+        update_window(w_window);
+        SDL_Delay(4);
     }
 
-    window = destroy_window(window);
+    w_window = destroy_window(w_window);
 
     quit_graphics();
 
