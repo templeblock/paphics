5befec6b0386c772bd2bff2c75749e1762eca56a
Sancho-Panza <leo@ndrs.fr>
2016-04-02 13:33:20 +0200

    Add equals functions to compare points

--- src/point.c
+++ src/point.c
@@ -1,5 +1,20 @@
 #include "point.h"
 
+bool equals(Point p1, Point p2) {
+
+    bool eq;
+
+    eq = true;
+
+    if (p1.x != p2.x) {
+        eq = false;
+    } else if (p1.y != p2.y) {
+        eq = false;
+    }
+
+    return eq;
+}
+
 int point_distance(Point a, Point b) {
 
     int distance;
--- src/point.h
+++ src/point.h
@@ -2,11 +2,14 @@
 #define DEF_POINT_H
 
 #include <math.h>
+#include <stdbool.h>
 
 typedef struct {
     int x;
     int y;
 } Point;
+
+bool equals(Point p1, Point p2);
 
 int point_distance(Point a, Point b);
 
