d635b149fa8e6a5451d09975fc3859d5bb0d7777
Sancho-Panza <leo@ndrs.fr>
2016-04-12 12:44:54 +0200

    Update sphere

--- src/sphere.c
+++ src/sphere.c
@@ -1,1 +1,44 @@
 #include "sphere.h"
+
+void sphere_draw_fill(Surface surface, Sphere* sphere) {
+
+    int i;
+    int nb_step;
+
+    int dred;
+    int dgreen;
+    int dblue;
+    int dradius;
+
+    Uint32 newColor;
+    SDL_Color color;
+    SDL_Color colorTmp;
+    Point pointTmp;
+    int radiusTmp;
+
+    color = translate_color(sphere->color);
+
+    nb_step = sphere->radius * 0.5;
+
+    dred = (255 - color.r) / nb_step;
+    dgreen = (255 - color.g) / nb_step;
+    dblue = (255 - color.b) / nb_step;
+
+    dradius = sphere->radius / nb_step;
+
+    for (i = 0; i < nb_step; i++) {
+
+        colorTmp.r = color.r + dred * i;
+        colorTmp.g = color.g + dgreen * i;
+        colorTmp.b = color.b + dblue * i;
+
+        pointTmp.x = sphere->center.x + i;
+        pointTmp.y = sphere->center.y + i;
+
+        radiusTmp = sphere->radius - dradius * i;
+
+        newColor = SDL_MapRGB(surface.surface->format, colorTmp.r, colorTmp.g, colorTmp.b);
+
+        circle_draw_fill(surface, pointTmp, radiusTmp, newColor);
+    }
+}
--- src/sphere.h
+++ src/sphere.h
@@ -3,11 +3,15 @@
 
 #include <SDL2/SDL.h>
 #include "point.h"
+#include "surface.h"
+#include "circle.h"
 
 typedef struct {
     Point center;
     int radius;
     Uint32 color;
 } Sphere;
+
+void sphere_draw_fill(Surface surface, Sphere* sphere);
 
 #endif
