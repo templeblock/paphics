59d3807a247cf39596eaee6f44fd624f1c3f85cc
Sancho-Panza <leo@ndrs.fr>
2016-04-12 12:46:30 +0200

    Remove almost all specific draw functions

--- src/draw.h
+++ src/draw.h
@@ -1,34 +1,14 @@
 #ifndef DEF_DRAW_H
 #define DEF_DRAW_H
 
-#include <SDL2/SDL.h>
-#include "graphics.h"
-#include "surface.h"
 #include "point.h"
 #include "pixel.h"
-#include "sphere.h"
 #include "line.h"
+#include "rectangle.h"
+#include "quadrilateral.h"
+#include "circle.h"
+#include "sphere.h"
 
-void draw(Surface surface, ...);
-
-void draw_borders_in(Surface surface, Uint32 color);
-
-void draw_borders_out(Surface surface, Uint32 color);
-
-void draw_circle(Surface surface, Point center, int radius, Uint32 color);
-
-void draw_fill_circle(Surface surface, Point center, int radius, Uint32 color);
-
-void draw_fill_rectangle(Surface surface, Point a, Point b, Uint32 color);
-
-void draw_fill_sphere(Surface surface, Sphere* sphere);
-
-void draw_line(Surface surface, Line* line);
-
-void draw_pixel(Surface surface, Pixel* pixel);
-
-void draw_quadrilateral(Surface surface, Point p1, Point p2, Point p3, Point p4, Uint32 color);
-
-void draw_rectangle(Surface surface, Point a, Point b, Uint32 color);
+/*void draw(Surface surface, ...);*/
 
 #endif
