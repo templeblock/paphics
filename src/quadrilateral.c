6e9b0781868b1d2ef1af9136635c274ac98273e0
Sancho-Panza <leo@ndrs.fr>
2016-04-12 22:21:33 +0200

    Update draw_quadrilateral

--- src/quadrilateral.c
+++ src/quadrilateral.c
@@ -1,6 +1,6 @@
 #include "quadrilateral.h"
 
-void quadrilateral_draw(Surface surface, Point p1, Point p2, Point p3, Point p4, Uint32 color) {
+/*void quadrilateral_draw_old(Surface surface, Point p1, Point p2, Point p3, Point p4, Uint32 color) {
 
     Point max_left;
     Point max_right;
@@ -63,6 +63,102 @@ void quadrilateral_draw(Surface surface, Point p1, Point p2, Point p3, Point p4,
 
     line4.a = other2;
     line4.b = max_left;
+
+    line1.color = color;
+    line2.color = color;
+    line3.color = color;
+    line4.color = color;
+
+    line_draw(surface, &line1);
+    line_draw(surface, &line2);
+    line_draw(surface, &line3);
+    line_draw(surface, &line4);
+
+}*/
+
+void quadrilateral_draw(Surface surface, Point p1, Point p2, Point p3, Point p4, Uint32 color) {
+
+    Point leftToRight[4];
+    Point topToBottom[4];
+    Point tmp;
+    Line line1;
+    Line line2;
+    Line line3;
+    Line line4;
+
+    point_sort_leftToRight(4, leftToRight, p1, p2, p3, p4);
+    point_sort_topToBottom(4, topToBottom, p1, p2, p3, p4);
+
+    printf("\nGauche[0]: (%d, %d)", leftToRight[0].x, leftToRight[0].y);
+    printf("\nGauche[1]: (%d, %d)", leftToRight[1].x, leftToRight[1].y);
+    printf("\nGauche[2]: (%d, %d)", leftToRight[2].x, leftToRight[2].y);
+    printf("\nGauche[3]: (%d, %d)\n", leftToRight[3].x, leftToRight[3].y);
+
+    printf("\nHaut[0]: (%d, %d)", topToBottom[0].x, topToBottom[0].y);
+    printf("\nHaut[1]: (%d, %d)", topToBottom[1].x, topToBottom[1].y);
+    printf("\nHaut[2]: (%d, %d)", topToBottom[2].x, topToBottom[2].y);
+    printf("\nHaut[3]: (%d, %d)\n", topToBottom[3].x, topToBottom[3].y);
+
+    if (equals(leftToRight[0], topToBottom[3])) {
+
+        line1.a = leftToRight[0];
+        line1.b = leftToRight[1];
+
+        line2.a = leftToRight[0];
+        line2.b = topToBottom[2];
+
+        if (equals(line1.a, leftToRight[2]) || equals(line1.b, leftToRight[2]) || equals(line2.b, leftToRight[2])) {
+            tmp = leftToRight[3];
+        } else {
+            tmp = leftToRight[4];
+        }
+
+        line3.a = topToBottom[2];
+        line3.b = tmp;
+
+        line4.a = leftToRight[1];
+        line4.b = tmp;
+
+    } else if (equals(leftToRight[0], topToBottom[0])) {
+
+        line1.a = leftToRight[0];
+        line1.b = leftToRight[1];
+
+        line2.a = leftToRight[0];
+        line2.b = topToBottom[1];
+
+        if (equals(line1.a, leftToRight[2]) || equals(line1.b, leftToRight[2]) || equals(line2.b, leftToRight[2])) {
+            tmp = leftToRight[3];
+        } else {
+            tmp = leftToRight[4];
+        }
+
+        line3.a = topToBottom[1];
+        line3.b = tmp;
+
+        line4.a = leftToRight[1];
+        line4.b = tmp;
+
+    } else {
+
+        line1.a = leftToRight[0];
+        line1.b = topToBottom[0];
+
+        line2.a = leftToRight[0];
+        line2.b = topToBottom[3];
+
+        if (equals(line1.a, topToBottom[1])) {
+            tmp = topToBottom[2];
+        } else {
+            tmp = topToBottom[1];
+        }
+
+        line3.a = tmp;
+        line3.b = topToBottom[0];
+
+        line4.a = tmp;
+        line4.b = topToBottom[3];
+    }
 
     line1.color = color;
     line2.color = color;
