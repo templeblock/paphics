c62596d14946d823970f5ffbe8dc4bcc520ee9ae
Sancho-Panza <leo@ndrs.fr>
2016-04-16 01:34:07 +0200

    Clean code...

--- src/main.c
+++ src/main.c
@@ -21,118 +21,84 @@ void demo() {
     graphics_init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     atexit(SDL_Quit);
 
-    Point screen_size = graphics_get_screen_size();
-    int dx;
-    int dy;
-    int radius;
-    Surface plate;
     Window w_window;
-    Surface window;
-    Point window_position;
-    Event input;
-    Uint32 color;
-    Uint32 color1;
-    Uint32 color2;
-    Uint32 color3;
-    Uint32 color5;
-    Uint32 color7;
-    Uint32 color11;
-    Uint32 color13;
+    Point window_position = {50, 50};
+    Point screen_size = graphics_get_screen_size();
 
-    Point plate_origin;
-    Point plate_size;
-    Sphere sphere1;
-    Sphere sphere2;
-    Sphere sphere3;
-    Sphere sphere4;
-
-    Sphere swindow1;
-    Sphere swindow2;
-    Sphere swindow3;
-    Sphere swindow4;
-
-
-    input = event_create();
     window_create(&w_window, "Démo", &window_position, &screen_size, SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN);
+
+    Surface window;
 
     surface_create_from_window(&window, &w_window);
 
-    plate_size = window.size;
+    Point plate_size = window.size;
     plate_size.x /= 2;
     plate_size.y /= 2;
 
-    plate_origin = window.size;
-    plate_origin.x /= 4;
-    plate_origin.y /= 4;
+    Point plate_origin = plate_size;
+    plate_origin.x /= 2;
+    plate_origin.y /= 2;
+
+    Surface plate;
 
     surface_create(&plate, &plate_size, &plate_origin, &window);
 
-    sphere1.center.x = plate.size.x / 2;
-    sphere1.center.y = plate.size.y / 2;
+    Event input = input = event_create();;
 
-    dx = 0;
-    dy = 0;
-    color = calc_alea_int(0x000000, 0xFFFFFF);
-
-    radius = 2 * window.size.x / plate.size.x;
-    sphere1.radius = radius;
-    sphere2.radius = radius;
-    sphere3.radius = radius;
-    sphere4.radius = radius;
-
-    color1 = calc_alea_int(0x000000, 0xFFFFFF);
-    color2 = calc_alea_int(0x000000, 0xFFFFFF);
-    color3 = calc_alea_int(0x000000, 0xFFFFFF);
-    color5 = calc_alea_int(0x000000, 0xFFFFFF);
-    color7 = calc_alea_int(0x000000, 0xFFFFFF);
-    color11 = calc_alea_int(0x000000, 0xFFFFFF);
-    color13 = calc_alea_int(0x000000, 0xFFFFFF);
-
-    color = calc_alea_int(0x000000, 0xFFFFFF);
-
-    int colorPrem;
-    Pixel pix1;
-    Pixel pix2;
-    Pixel pix3;
-    Pixel pix4;
-    int radius_circle;
+    int dx = 0;
+    int dy = 0;
 
     Circle circle1;
     Circle circle2;
     Circle circle3;
     Circle circle4;
 
-    radius_circle = 1;
+    circle1.center.x = plate.size.x / 2;
+    circle1.center.y = plate.size.y / 2;
 
-    circle1.radius = radius_circle;
-    circle2.radius = radius_circle;
-    circle3.radius = radius_circle;
-    circle4.radius = radius_circle;
+    circle1.radius = 1;
+    circle2.radius = circle1.radius;
+    circle3.radius = circle1.radius;
+    circle4.radius = circle1.radius;
 
+    Sphere sphere1;
+
+    sphere1.radius = 8 * window.size.x / plate.size.x;;
+
+    int maxvar = (int) 8 * circle1.radius / (circle1.radius + circle1.radius);
     int dd;
-    int maxvar = 6 * radius_circle;
+    int colorPrem;
+
+    Uint32 color;
+    const Uint32 color1 = calc_alea_int(0x000000, 0xFFFFFF);
+    const Uint32 color2 = calc_alea_int(0x000000, 0xFFFFFF);
+    const Uint32 color3 = calc_alea_int(0x000000, 0xFFFFFF);
+    const Uint32 color5 = calc_alea_int(0x000000, 0xFFFFFF);
+    const Uint32 color7 = calc_alea_int(0x000000, 0xFFFFFF);
+    const Uint32 color11 = calc_alea_int(0x000000, 0xFFFFFF);
+    const Uint32 color13 = calc_alea_int(0x000000, 0xFFFFFF);
 
     while (!input.quit) {
 
         input = event_update(&input);
 
         dd = calc_alea_int(1, 7);
-        if (dd == 7) {
+
+        switch (dd) {
+        case 7:
             dd = 13;
-        } else if (dd == 6) {
+            break;
+        case 6:
             dd = 11;
-        } else if (dd == 5) {
+            break;
+        case 5:
             dd = 7;
-        } else if (dd == 4) {
+            break;
+        case 4:
             dd = 5;
-        } else if (dd == 3) {
-            dd = 3;
-        } else if (dd == 2) {
-            dd = 2;
-        } else if (dd == 1) {
-            dd = 1;
-        } else {
-            dd = 0;
+            break;
+        default:
+            break; // in this case, it has the good value so...
         }
 
         if (calc_alea_int(0, 1) == 0) {
@@ -142,22 +108,22 @@ void demo() {
         }
 
         dd = calc_alea_int(1, 7);
-        if (dd == 7) {
+
+        switch (dd) {
+        case 7:
             dd = 13;
-        } else if (dd == 6) {
+            break;
+        case 6:
             dd = 11;
-        } else if (dd == 5) {
+            break;
+        case 5:
             dd = 7;
-        } else if (dd == 4) {
+            break;
+        case 4:
             dd = 5;
-        } else if (dd == 3) {
-            dd = 3;
-        } else if (dd == 2) {
-            dd = 2;
-        } else if (dd == 1) {
-            dd = 1;
-        } else {
-            dd = 0;
+            break;
+        default:
+            break; // in this case, it has the good value so...
         }
 
         if (calc_alea_int(0, 1) == 0) {
@@ -166,8 +132,8 @@ void demo() {
             dy -= dd;
         }
 
-        dx += input.arrows.x;
-        dy += input.arrows.y;
+        dx += 2 * input.arrows.x;
+        dy += 2 * input.arrows.y;
 
         //dx++;
         //dy++;
@@ -186,37 +152,27 @@ void demo() {
             dx++;
         }
 
-        if (collision_is_out_of_surface_x(&plate, sphere1.center.x + dx, sphere1.radius)) {
+        if (collision_is_out_of_surface_x(&plate, circle1.center.x + dx, circle1.radius)) {
             dx = -dx;
         }
 
-        if (collision_is_out_of_surface_y(&plate,sphere1.center.y + dy, sphere1.radius)) {
+        if (collision_is_out_of_surface_y(&plate, circle1.center.y + dy, circle1.radius)) {
             dy = -dy;
         }
 
-        sphere1.center.x += dx;
-        sphere1.center.y += dy;
-        sphere2.center.x = plate.size.x - sphere1.center.x;
-        sphere2.center.y = plate.size.y - sphere1.center.y;
-        sphere3.center.x = sphere1.center.x;
-        sphere3.center.y = plate.size.y - sphere1.center.y;
-        sphere4.center.x = plate.size.x - sphere1.center.x;
-        sphere4.center.y = sphere1.center.y;
+        circle1.center.x += dx;
+        circle1.center.y += dy;
+        circle2.center.x = plate.size.x - circle1.center.x;
+        circle2.center.y = plate.size.y - circle1.center.y;
+        circle3.center.x = circle1.center.x;
+        circle3.center.y = plate.size.y - circle1.center.y;
+        circle4.center.x = plate.size.x - circle1.center.x;
+        circle4.center.y = circle1.center.y;
 
-        sphere1.color = color;
-        sphere2.color = color;
-        sphere3.color = color;
-        sphere4.color = color;
-
-        pix1.position = sphere1.center;
-        pix2.position = sphere2.center;
-        pix3.position = sphere3.center;
-        pix4.position = sphere4.center;
-
-        if (sphere1.center.x > plate.size.x / 2) {
-            colorPrem = sphere1.center.x;
+        if (circle1.center.x > plate.size.x / 2) {
+            colorPrem = circle1.center.x;
         } else {
-            colorPrem = sphere2.center.x;
+            colorPrem = circle2.center.x;
         }
 
         if (colorPrem % 13 == 0) {
@@ -235,41 +191,15 @@ void demo() {
             color = color1;
         }
 
-        pix1.color = color;
-        pix2.color = color;
-        pix3.color = color;
-        pix4.color = color;
-
-        circle1.center = pix1.position;
-        circle2.center = pix2.position;
-        circle3.center = pix3.position;
-        circle4.center = pix4.position;
-
         circle1.color = color;
         circle2.color = color;
         circle3.color = color;
         circle4.color = color;
 
-        //draw_quadrilateral(plate, sphere1.center, sphere2.center, sphere3.center, sphere4.center, color);
-        color = calc_alea_int(0x000000, 0xFFFFFF);
-
-        swindow1 = sphere1;
-        swindow2 = sphere2;
-        swindow3 = sphere3;
-        swindow4 = sphere4;
-
-        swindow1.center.x *= 2;
-        swindow1.center.y *= 2;
-        swindow1.radius *= 2;
-        swindow2.center.x *= 2;
-        swindow2.center.y *= 2;
-        swindow2.radius *= 2;
-        swindow3.center.x *= 2;
-        swindow3.center.y *= 2;
-        swindow3.radius *= 2;
-        swindow4.center.x *= 2;
-        swindow4.center.y *= 2;
-        swindow4.radius *= 2;
+        sphere1.color = color;
+        sphere1.center = circle1.center;
+        sphere1.center.x *= 2;
+        sphere1.center.y *= 2;
 
         surface_clear(&window);
 
@@ -280,23 +210,12 @@ void demo() {
         surface_draw_borders_in(&window, 0xFFFFFF);
         surface_draw_borders_out(&plate, 0xFFFFFF);
 
-        /*pixel_draw(plate, &pix1);
-        pixel_draw(plate, &pix2);
-        pixel_draw(plate, &pix3);
-        pixel_draw(plate, &pix4);*/
-
         circle_draw_fill(&plate, &circle1);
         circle_draw_fill(&plate, &circle2);
         circle_draw_fill(&plate, &circle3);
         circle_draw_fill(&plate, &circle4);
 
-        //  quadrilateral_draw(plate, pix1.position, pix2.position, pix3.position, pix4.position, pix1.color);
-
-
-        sphere_draw_fill(&window, &swindow1);
-        //sphere_draw_fill(&window, &swindow2);
-        //sphere_draw_fill(&window, &swindow3);
-        //sphere_draw_fill(&window, &swindow4);
+        sphere_draw_fill(&window, &sphere1);
 
         surface_blit(&plate);
         window_update(&w_window);
