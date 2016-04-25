ab264b427a29ad7c4966fd10698cc90bbe7fd078
Sancho-Panza <leo@ndrs.fr>
2016-04-26 01:31:24 +0200

    Update pixel_draw

--- src/pixel.c
+++ src/pixel.c
@@ -11,7 +11,10 @@ Pixel pixel_create() {
 
 void pixel_draw(Canvas* canvas, Pixel* pixel) {
 
-    if (pixel->position.x > 0 && pixel->position.y > 0 && pixel->position.x < canvas->surface->w && pixel->position.y < canvas->surface->h) {
-        *((Uint32*) canvas->surface->pixels + (canvas->surface->h - pixel->position.y - 1) * canvas->surface->w + pixel->position.x) = pixel->color;
+    if (pixel->position.x < 0 || pixel->position.y < 0 || pixel->position.x > canvas->size.x || pixel->position.y > canvas->size.y) {
+        fprintf(stderr, "\nWarning: trying to draw a pixel outside of the canvas: pixel (%d, %d)\n", pixel->position.x, pixel->position.y);
+    } else {
+        *((Uint32*) canvas->surface->pixels + (canvas->size.y - pixel->position.y - 1) * canvas->size.x + pixel->position.x) = pixel->color;
     }
+
 }
