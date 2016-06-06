ae761676e321fc15ef5da8f1210e5320251a5915
zapashcanon <leo@ndrs.fr>
2016-06-06 04:33:47 +0200

    Add color files.

---
+++ head/color.h
@@ -0,0 +1,8 @@
+#ifndef DEF_COLOR_H
+#define DEF_COLOR_H
+
+#include <SDL2/SDL.h>
+
+void color_translate(Uint32 int_color, SDL_Color* color);
+
+#endif
---
+++ src/color.c
@@ -0,0 +1,21 @@
+#include "color.h"
+
+void color_translate(Uint32 int_color, SDL_Color* color) {
+
+    #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    color->r = (int_color & 0x00ff0000) / 0x10000;
+    color->g = (int_color & 0x0000ff00) / 0x100;
+    color->b = int_color & 0x000000ff;
+    #else
+    color->r = (int_color & 0x000000ff);
+    color->g = (Uint8) ((int_color & 0x0000ff00) / 0x100);
+    color->b = (Uint8) ((int_color & 0x00ff0000) / 0x10000);
+    #endif
+    
+    color->a = 0;
+    
+    // shouldn't have to do this... would be good to find why
+    color->r = (Uint8) (255 - color->r);
+    color->g = (Uint8) (255 - color->g);
+    color->b = (Uint8) (255 - color->b);
+}
