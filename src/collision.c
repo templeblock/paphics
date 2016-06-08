6139a3665eaecae503d590047b9f79512d059e61
zapashcanon <leo@ndrs.fr>
2016-06-08 19:50:20 +0200

    Better collision detection algo.

--- src/collision.c
+++ src/collision.c
@@ -2,45 +2,45 @@
 
 bool collision_is_out_of_canvas(Canvas* canvas, Point a, int radius) {
 
-    bool is_out = false;
-    
+    bool is_out;
+
     if (collision_is_out_of_canvas_x(canvas, a.x, radius)) {
         is_out = true;
-    }
-    
-    if (collision_is_out_of_canvas_y(canvas, a.y, radius)) {
+    } else if (collision_is_out_of_canvas_y(canvas, a.y, radius)) {
         is_out = true;
+    } else {
+        is_out = false;
     }
-    
+
     return is_out;
 }
 
 bool collision_is_out_of_canvas_x(Canvas* canvas, int a, int radius) {
 
     bool is_out;
-    
-    is_out = false;
-    
+
     if (a + radius > canvas->size.x) {
         is_out = true;
     } else if (a - radius < 0) {
         is_out = true;
+    } else {
+      is_out = false;
     }
-    
+
     return is_out;
 }
 
 bool collision_is_out_of_canvas_y(Canvas* canvas, int a, int radius) {
 
     bool is_out;
-    
-    is_out = false;
-    
+
     if (a + radius > canvas->size.y) {
         is_out = true;
     } else if (a - radius < 0) {
         is_out = true;
+    } else {
+      is_out = false;
     }
-    
+
     return is_out;
 }
