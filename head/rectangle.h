1ab6196b67180f17b3e49a5f49023abe7eaedaa5
zapashcanon <leo@ndrs.fr>
2016-06-16 21:11:31 +0200

    Add the new rectangle_draw function.

--- head/rectangle.h
+++ head/rectangle.h
@@ -13,4 +13,6 @@ void rectangle_draw(Canvas* canvas, const Point a, const Point b, const Uint32 c
 
 void rectangle_draw_fill(Canvas* canvas, const Point a, const Point b, const Uint32 color);
 
+void rectangle_draw_new(Canvas* canvas, const Rectangle* rectangle, const Uint32 color);
+
 #endif
