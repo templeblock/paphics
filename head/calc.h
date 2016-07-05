050240b0e9c30b88ab80a16bb8a685b409005f5c
zapashcanon <leo@ndrs.fr>
2016-07-05 17:35:59 +0200

    Add calc_* functions doc

--- head/calc.h
+++ head/calc.h
@@ -4,8 +4,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * \fn float calc_alea_float(void)
+ * \brief Function to get a random float x in [0 ; 1[.
+ * \return The random float.
+ */
 float calc_alea_float(void);
 
+/**
+ * \fn int calc_alea_int(const int min, const int max)
+ * \brief Function to get a random int.
+ * \param min The minimun value for the random int.
+ * \param max The maximum value for the random int.
+ * \return The random int.
+ */
 int calc_alea_int(const int min, const int max);
 
 #endif
