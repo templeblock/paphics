0cf8dc6a947bc0ec6cd60c1d5796733cef8ebf99
zapashcanon <leo@ndrs.fr>
2016-07-06 20:13:47 +0200

    Add pixel.h doc

--- head/pixel.h
+++ head/pixel.h
@@ -1,13 +1,29 @@
+/**
+ * \file line.h
+ * \brief Everything related to Pixel.
+ */
+
 #ifndef DEF_PIXEL_H
 #define DEF_PIXEL_H
 
 #include "canvas.h"
 
+/**
+ * \struct Pixel
+ * \brief A struct used to represent a pixel.
+ */
 typedef struct {
-    Point position;
-    Canvas* canvas;
+    Point position; /*!< Point representing the position of the Pixel. */
+    Canvas* canvas; /*!< Pointer to the Canvas the Pixel belongs to. */
 } Pixel;
 
+/**
+ * \fn void pixel_draw(const Pixel* pixel, const Color* color)
+ * \brief Function to draw a pixel.
+ *
+ * \param line A pointer to the Pixel to draw.
+ * \param color A pointer to the Color to use to draw the Pixel.
+ */
 void pixel_draw(const Pixel* pixel, const Color* color);
 
 #endif
