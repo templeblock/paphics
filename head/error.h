804e98ff523b7020e137ed921261f996e2cd7490
zapashcanon <leo@ndrs.fr>
2016-07-06 16:55:17 +0200

    Add error.h doc

--- head/error.h
+++ head/error.h
@@ -1,9 +1,18 @@
+/**
+ * \file error.h
+ * \brief Everything related to errors and warnings handling.
+ */
+
 #ifndef DEF_ERROR_H
 #define DEF_ERROR_H
 
 #include <SDL2/SDL_image.h>
 #include "startstop.h"
 
+/**
+ * \fn void error_quit(void) __attribute__ ((noreturn))
+ * \brief Function to quit after an error, will stop graphics and SDL components and stop the program.
+ */
 void error_quit(void) __attribute__ ((noreturn));
 
 #endif
