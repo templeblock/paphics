ce0324f1c939e25bbd7fbb9381b2dc918fd6f424
zapashcanon <leo@ndrs.fr>
2016-07-07 01:05:42 +0200

    Add screen.h doc

--- head/screen.h
+++ head/screen.h
@@ -1,9 +1,20 @@
+/**
+ * \file screen.h
+ * \brief Everything related to the screen.
+ */
+
 #ifndef DEF_SCREEN_H
 #define DEF_SCREEN_H
 
 #include "error.h"
 #include "point.h"
 
+/**
+ * \fn void screen_get_size(Point* screenSize)
+ * \brief Function to get the screen's size.
+ *
+ * \param screenSize A pointer to the Point in which the screen's size must be stored.
+ */
 void screen_get_size(Point* screenSize);
 
 #endif
