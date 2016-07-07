b985fc3834a6bb0d0fd4c1e17216830c4cadf56e
zapashcanon <leo@ndrs.fr>
2016-07-07 17:30:56 +0200

    Clean sphere_draw_fill function

--- src/sphere.c
+++ src/sphere.c
@@ -2,39 +2,26 @@
 
 void sphere_draw_fill(const Sphere* sphere, const Color* color) {
 
+    int nb_step = (int) (((float) sphere->radius) * 0.5f);
+    
+    Uint8 colorRed = color_get_red(color);
+    Uint8 colorGreen = color_get_green(color);
+    Uint8 colorBlue = color_get_blue(color);
+    
+    int dred = (255 - colorRed) / nb_step;
+    int dgreen = (255 - colorGreen) / nb_step;
+    int dblue = (255 - colorBlue) / nb_step;
+    int dradius = sphere->radius / nb_step;
+    
     int i;
-    int nb_step;
-    
-    int dred;
-    int dgreen;
-    int dblue;
-    int dradius;
-    
     Uint8 tmpRed;
     Uint8 tmpGreen;
     Uint8 tmpBlue;
     
-    Uint8 colorRed;
-    Uint8 colorGreen;
-    Uint8 colorBlue;
-    
-    colorRed = color_get_red(color);
-    colorGreen = color_get_green(color);
-    colorBlue = color_get_blue(color);
+    Color colorTmp;
+    colorTmp.alpha = 0;
     
     Circle circle;
-    Color colorToDraw;
-    
-    colorToDraw.alpha = 0;
-    
-    nb_step = (int) (((float) sphere->radius) * 0.5f);
-    
-    dred = (255 - colorRed) / nb_step;
-    dgreen = (255 - colorGreen) / nb_step;
-    dblue = (255 - colorBlue) / nb_step;
-    
-    dradius = sphere->radius / nb_step;
-    
     circle.canvas = sphere->canvas;
     
     for (i = 0; i < nb_step; i++) {
@@ -48,8 +35,8 @@ void sphere_draw_fill(const Sphere* sphere, const Color* color) {
         
         circle.radius = sphere->radius - dradius * i;
         
-        colorToDraw.rgb = SDL_MapRGB(circle.canvas->surface->format, tmpRed, tmpGreen, tmpBlue);
+        colorTmp.rgb = SDL_MapRGB(circle.canvas->surface->format, tmpRed, tmpGreen, tmpBlue);
         
-        circle_draw_fill(&circle, &colorToDraw);
+        circle_draw_fill(&circle, &colorTmp);
     }
 }
