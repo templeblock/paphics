9243708b89fcdb4e3f1aa91aad1e348eb3ae29c3
zapashcanon <leo@ndrs.fr>
2016-07-19 22:04:39 +0200

    Add point_distance tests

--- tests/point_test.c
+++ tests/point_test.c
@@ -2,7 +2,7 @@
 #include <check.h>
 #include <paphics/paphics.h>
 
-START_TEST(equals) {
+START_TEST(test_equals) {
 
     Point a;
     Point b;
@@ -22,7 +22,7 @@ START_TEST(equals) {
 }
 END_TEST
 
-START_TEST(not_equals) {
+START_TEST(test_not_equals) {
 
     Point a;
     Point b;
@@ -48,18 +48,71 @@ START_TEST(not_equals) {
 }
 END_TEST
 
-Suite* are_equals(void) {
+Suite* suite_are_equals(void) {
 
     Suite* s;
     TCase* tc_core;
 
-    s = suite_create("point_are_equals");
+    s = suite_create("suite_are_equals");
 
     /* Core test case */
     tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, equals);
-    tcase_add_test(tc_core, not_equals);
+    tcase_add_test(tc_core, test_equals);
+    tcase_add_test(tc_core, test_not_equals);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+START_TEST(test_distance) {
+
+  Point a;
+  Point b;
+
+  a.x = 342;
+  a.y = 2421;
+
+  b = a;
+
+  fail_unless(point_distance(a, b) == 0, "must be == 0");
+  fail_unless(point_distance(b, a) == 0, "must be == 0");
+
+  a.x = 1;
+  a.y = 1;
+
+  b.x = 1;
+  b.y = 2;
+
+  fail_unless(point_distance(a, b) == 1, "must be == 1");
+  fail_unless(point_distance(b, a) == 1, "must be == 1");
+
+  b.x = 0;
+  b.y = 0;
+
+  fail_unless(point_distance(a, b) == 1, "must be == 1");
+  fail_unless(point_distance(b, a) == 1, "must be == 1");
+
+  a.x = 3;
+  a.y = 4;
+
+  fail_unless(point_distance(a, b) == 5, "must be == 5");
+  fail_unless(point_distance(b, a) == 5, "must be == 5");
+
+}
+END_TEST
+
+Suite* suite_distance(void) {
+
+    Suite* s;
+    TCase* tc_core;
+
+    s = suite_create("suite_distance");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_distance);
     suite_add_tcase(s, tc_core);
 
     return s;
@@ -72,7 +125,14 @@ int main(void) {
     Suite* s;
     SRunner* sr;
 
-    s = are_equals();
+    s = suite_are_equals();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed += srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    s = suite_distance();
     sr = srunner_create(s);
 
     srunner_run_all(sr, CK_NORMAL);
