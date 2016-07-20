7f5f520d704b4e3d424e7076cf51c468ff31c1c7
zapashcanon <leo@ndrs.fr>
2016-07-20 03:11:21 +0200

    Fix pixel_draw

--- src/pixel.c
+++ src/pixel.c
@@ -2,7 +2,7 @@
 
 void pixel_draw(const Pixel* pixel, const Color* color) {
 
-    if (pixel->position.x < 0 || pixel->position.y < 0 || pixel->position.x > pixel->canvas->size.x || pixel->position.y > pixel->canvas->size.y) {
+    if (pixel->position.x < 0 || pixel->position.y < 0 || pixel->position.x >= pixel->canvas->size.x || pixel->position.y >= pixel->canvas->size.y) {
         fprintf(stderr, "\nWarning: trying to draw a pixel outside of the canvas: pixel (%d, %d)\n", pixel->position.x, pixel->position.y);
     } else {
         *((Uint32*) pixel->canvas->surface->pixels + (pixel->canvas->size.y - pixel->position.y - 1) * pixel->canvas->size.x + pixel->position.x) = color->rgb;
