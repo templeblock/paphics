df7dc1f4fc1e9ebaf141a0e7500d0102ca0ca1f0
zapashcanon <leo@ndrs.fr>
2016-07-21 19:55:57 +0200

    Fix color functions

--- src/color.c
+++ src/color.c
@@ -2,20 +2,12 @@
 
 Uint8 color_get_red(const Color* color) {
 
-    Uint8 red;
-    
-    red = (Uint8) ((color->rgb % 256) << 16);
-    
-    return red;
+    return (Uint8) (color->rgb >> 16);
 }
 
 Uint8 color_get_green(const Color* color) {
 
-    Uint8 green;
-    
-    green = (Uint8) ((color->rgb % 256) << 8);
-    
-    return green;
+    return (Uint8) ((color->rgb >> 8) % 256);
 }
 
 Uint8 color_get_blue(const Color* color) {
