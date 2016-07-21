2c7e8aa459d9cfb827d82ca38e56073d21532e7c
zapashcanon <leo@ndrs.fr>
2016-07-21 20:54:33 +0200

    Minor update

--- src/event.c
+++ src/event.c
@@ -5,13 +5,13 @@ void event_create(Event* newEvent) {
     newEvent->quit = false;
     newEvent->arrows.x = 0;
     newEvent->arrows.y = 0;
-    newEvent->space = 0;
+    newEvent->space = false;
 }
 
 void event_update(Event* event) {
 
     SDL_Event tmp;
-    
+
     while (SDL_PollEvent(&tmp) != 0) {
         if (tmp.type == SDL_QUIT) {
             event->quit = true;
@@ -20,27 +20,27 @@ void event_update(Event* event) {
                 case SDLK_UP:
                     event->arrows.y = 1;
                     break;
-                    
+
                 case SDLK_DOWN:
                     event->arrows.y = -1;
                     break;
-                    
+
                 case SDLK_LEFT:
                     event->arrows.x = -1;
                     break;
-                    
+
                 case SDLK_RIGHT:
                     event->arrows.x = 1;
                     break;
-                    
+
                 case SDLK_SPACE:
                     event->space = 1;
                     break;
-                    
+
                 case SDLK_ESCAPE:
                     event->quit = 1;
                     break;
-                    
+
                 default:
                     break;
             }
@@ -48,23 +48,23 @@ void event_update(Event* event) {
             switch (tmp.key.keysym.sym) {
                 case SDLK_UP:
                     event->arrows.y = 0;
-                    
+
                 //break;
                 case SDLK_DOWN:
                     event->arrows.y = 0;
-                    
+
                 //break;
                 case SDLK_LEFT:
                     event->arrows.x = 0;
-                    
+
                 //break;
                 case SDLK_RIGHT:
                     event->arrows.x = 0;
-                    
+
                 //break;
                 case SDLK_SPACE:
                     event->space = 0;
-                    
+
                 default:
                     break;
             }
