9eeaea7b7388b4c41b57b91f073754f36a74c39c
zapashcanon <leo@ndrs.fr>
2016-07-22 23:55:12 +0200

    Astyle

--- src/mouse.c
+++ src/mouse.c
@@ -18,23 +18,23 @@ void mouse_show(void) {
 bool mouse_is_hidden(void) {
 
     int mouseStatus;
-
+    
     mouseStatus = SDL_ShowCursor(-1);
-
+    
     if (mouseStatus < 0) {
         fprintf(stderr, "mouse_is_hidden failed: %s\n", SDL_GetError());
         error_quit();
     }
-
+    
     return (mouseStatus == 0 ? true : false);
 }
 
 bool mouse_is_shown(void) {
 
     bool isShown;
-
+    
     isShown = !mouse_is_hidden();
-
+    
     return isShown;
 }
 
@@ -42,9 +42,9 @@ void mouse_wait_click(const Window* window, Point* click) {
 
     bool wait;
     wait = true;
-
+    
     SDL_Event input;
-
+    
     while (SDL_WaitEvent(&input) && wait) {
         if (input.type == SDL_MOUSEBUTTONDOWN && input.button.button == SDL_BUTTON_LEFT) {
             wait = false;
@@ -52,5 +52,5 @@ void mouse_wait_click(const Window* window, Point* click) {
             click->y = window->size.y - input.button.y - 1; // not sure the -1 is correct here
         }
     }
-
+    
 }
