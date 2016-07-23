abc04063581e38a42306a78657573c47965691e7
zapashcanon <leo@ndrs.fr>
2016-07-23 20:18:18 +0200

    Add canvas_contains_point test

--- tests/canvas_test.c
+++ tests/canvas_test.c
@@ -6,96 +6,96 @@ START_TEST(test_canvas_collision_canvas) {
 
     Canvas c1;
     Canvas c2;
-    
+
     c1.size.x = 100;
     c1.size.y = 10;
-    
+
     c2.size.x = 20;
     c2.size.y = 200;
-    
+
     c1.origin.x = 1000;
     c1.origin.y = 1000;
-    
+
     c2.origin.x = 0;
     c2.origin.y = 0;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.x = 1000;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.x = 0;
     c2.origin.y = 1000;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.x = 2000;
     c2.origin.y = 0;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.y = 1000;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.y = 2000;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.x = 1000;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.x = 0;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.x = 980;
     c2.origin.y = 800;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.x = 981;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.x = 980;
     c2.origin.y = 801;
-    
+
     fail_if(canvas_collision_canvas(&c1, &c2), "should not collide");
     fail_if(canvas_collision_canvas(&c2, &c1), "should not collide");
-    
+
     c2.origin.x = 981;
-    
+
     fail_unless(canvas_collision_canvas(&c1, &c2), "should collide");
     fail_unless(canvas_collision_canvas(&c2, &c1), "should collide");
-    
+
     c2.origin.x = 981;
     c2.origin.y = 1009;
-    
+
     fail_unless(canvas_collision_canvas(&c1, &c2), "should collide");
     fail_unless(canvas_collision_canvas(&c2, &c1), "should collide");
-    
+
     c2.origin.x = 1099;
     c2.origin.y = 801;
-    
+
     fail_unless(canvas_collision_canvas(&c1, &c2), "should collide");
     fail_unless(canvas_collision_canvas(&c2, &c1), "should collide");
-    
+
     c2.origin.x = 1099;
     c2.origin.y = 1009;
-    
+
     fail_unless(canvas_collision_canvas(&c1, &c2), "should collide");
     fail_unless(canvas_collision_canvas(&c2, &c1), "should collide");
 }
@@ -105,31 +105,124 @@ Suite* suite_canvas_collision_canvas(void) {
 
     Suite* s;
     TCase* tc_core;
-    
+
     s = suite_create("suite_canvas_collision_canvas");
-    
+
     /* Core test case */
     tc_core = tcase_create("Core");
-    
+
     tcase_add_test(tc_core, test_canvas_collision_canvas);
     suite_add_tcase(s, tc_core);
-    
+
     return s;
+}
+
+START_TEST(test_canvas_contains_point) {
+
+  Canvas canvas;
+  Point p;
+
+  canvas.origin.x = 100;
+  canvas.origin.y = 200;
+
+  canvas.size.x = 200;
+  canvas.size.y = 100;
+
+  p.x = 0;
+  p.y = 0;
+
+  fail_if(canvas_contains_point(&canvas, p), "should be false");
+
+  p.x = 99;
+  p.y = 199;
+
+  fail_if(canvas_contains_point(&canvas, p), "should be false");
+
+  p.y = 200;
+
+  fail_if(canvas_contains_point(&canvas, p), "should be false");
+
+  p.x = 100;
+  p.y = 199;
+
+  fail_if(canvas_contains_point(&canvas, p), "should be false");
+
+  p.x = 300;
+  p.y = 300;
+
+  fail_if(canvas_contains_point(&canvas, p), "should be false");
+
+  p.x = 299;
+
+  fail_if(canvas_contains_point(&canvas, p), "should be false");
+
+  p.x = 300;
+  p.y = 299;
+
+  fail_if(canvas_contains_point(&canvas, p), "should be false");
+
+  p.x = 99;
+
+  fail_if(canvas_contains_point(&canvas, p), "should be false");
+
+  p.x = 100;
+  p.y = 200;
+
+  fail_unless(canvas_contains_point(&canvas, p), "should be true");
+
+  p.x = 299;
+  p.y = 299;
+
+  fail_unless(canvas_contains_point(&canvas, p), "should be true");
+
+  p.x = 100;
+
+  fail_unless(canvas_contains_point(&canvas, p), "should be true");
+
+  p.x = 250;
+  p.y = 250;
+
+  fail_unless(canvas_contains_point(&canvas, p), "should be true");
+
+}
+END_TEST
+
+Suite* suite_canvas_contains_point(void) {
+
+      Suite* s;
+      TCase* tc_core;
+
+      s = suite_create("suite_canvas_contains_point");
+
+      /* Core test case */
+      tc_core = tcase_create("Core");
+
+      tcase_add_test(tc_core, test_canvas_contains_point);
+      suite_add_tcase(s, tc_core);
+
+      return s;
 }
 
 int main(void) {
 
     int number_failed = 0;
-    
+
     Suite* s;
     SRunner* sr;
-    
+
     s = suite_canvas_collision_canvas();
     sr = srunner_create(s);
-    
+
     srunner_run_all(sr, CK_NORMAL);
     number_failed += srunner_ntests_failed(sr);
     srunner_free(sr);
-    
+
+    s = suite_canvas_contains_point();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed += srunner_ntests_failed(sr);
+    srunner_free(sr);
+
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
